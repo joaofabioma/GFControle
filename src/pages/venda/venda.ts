01c59dfbb45a093a901d030299157a24c94b6583
joaofabioma <amarall.jf@gmail.com>
2017-11-13 19:28:16 -0300

    ident

--- src/pages/venda/venda.ts
+++ src/pages/venda/venda.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the VendaPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @Component({
   selector: 'page-venda',
   templateUrl: 'venda.html',
@@ -15,10 +8,6 @@ import { NavController, NavParams } from 'ionic-angular';
 export class VendaPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad VendaPage');
   }
 
 }
